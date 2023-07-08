75b7c73fb8074993b95f0894a9f7227e32f8832a
Natsume <114702561+NatsumeRyou@users.noreply.github.com>
2023-07-07 21:10:05 -0300

    Delete 5 directory

--- 5/InvertString.py
+++
@@ -1,9 +0,0 @@
-def inverte_string(s):
-    resultado = ''
-    for i in range(len(s) - 1, -1, -1):
-        resultado += s[i]
-    return resultado
-
-string_original = input('Digite uma string: ')
-string_invertida = inverte_string(string_original)
-print('A string intertida é:', string_invertida)
